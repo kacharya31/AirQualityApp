f880ab758ffd36627a8d74b86f8e8b497451ae43
Tejas Mirthipati <65861825+Tezmir@users.noreply.github.com>
2023-12-03 03:40:25 -0500

    fixed csv files

--- popscraper.py
+++ popscraper.py
@@ -172,12 +172,11 @@ gas_data_df = gas_data_df[(gas_data_df['Year'] >= 2000) & (gas_data_df['Year'] <
 
 gas_data_df['State'] = gas_data_df['State'].map(state_names_to_abbreviations)
 
+petroleum_data_df = petroleum_data_df.dropna(subset = ['State'])
+coal_long_format = coal_long_format.dropna(subset = ['State'])
+gas_data_df = gas_data_df.dropna(subset = ['State'])
+
 population_density_df.to_csv('population.csv')
 petroleum_data_df.to_csv('petroleum.csv')
 coal_long_format.to_csv('coal.csv')
 gas_data_df.to_csv('gas_data.csv')
-
-# print(population_density_df)
-# print(petroleum_data_df)
-# print(coal_long_format)
-# print(gas_data_df)
